a7759c9032a4ef2832270919ba32c0fb244e343e
Will Beasley <wibeasley@hotmail.com>
2022-01-12 14:12:33 -0600

    updates

---
+++ 2494-wallaby/KISS/huey-dewey-and-wallaby/figure eight 2nd vrs/src/main.c
@@ -0,0 +1,41 @@
+#include <kipr/botball.h>
+
+int move () {
+    printf ("%s\n");
+    return 0;
+}
+
+int main()
+{
+//    printf("YAAAASSSSS GOOOOOOOO LIIIIKE A BALLEERRRIIINAAAA GUUUUURRRRLLL\n");
+//    motor(0, 30);
+//    motor(3, 31);
+//    msleep(7500);
+    
+//   printf("TUUUUURRRRRRNNNNN BBBOOOOOIIIIIIIII\n"); 
+//   motor(0, 39); 
+//   motor(3, 21);
+//   msleep(5393); 
+    
+//   printf("BABA GRILLLLLLLLLL AAAAAAAAAAAAAAAAAAAAAAAA\n");
+//    motor(0, 21);
+//    motor(3, 44);
+//    msleep(19946); 
+    
+//    printf("OOOMMMMMGGGGGG GOOOOO LIKE THE QUEEENG YOU ISSSSS");
+//    motor(0, 34);
+//    motor(0, 35);
+//    msleep(4330);
+    
+//    printf("GGGGGGOOOOOOOO BBAABBYYYY YOOOUUURRRR ALMOOOOSSSTT THEERRERE");
+//    motor(0, 45);
+//    motor(3, 18);
+//    msleep(6275);       
+    
+//    printf("SLAYYYYYYYYY S- AHHHHHHHHHHHHHHHHHHHHHHH wow that was a reacton-\n");
+//    motor(0, 18);
+//    motor(3, 45); 
+//    msleep(8888);
+ 
+    return 0;
+}
